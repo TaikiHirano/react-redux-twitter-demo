5038d650be5f3dc1a7f6eda28bf766245077673f
hirano <rhi691508@gmail.com>
2023-09-13 16:46:26 +0900

    feat: Process to get user information in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,28 @@ import { selectUser, login, logout } from "./features/userSlice";
 import { auth } from "./firebase";
 
 const App: React.FC = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoURL,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    return () => {
+      unSub();
+    };
+  }, [dispatch]);
+
   return <div className="App"></div>;
 };
 
