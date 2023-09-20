f2d427cd955331c037d1b58278d8bc1a1218e84e
hirano <rhi691508@gmail.com>
2023-09-20 23:25:56 +0900

    feat: Correct spelling errors

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,10 +5,10 @@ import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMEIN,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storegeBuket: process.env.REACT_APP_FIREBASE_STORAGE_BACKET,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
